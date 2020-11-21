6a28cbd79c03a896301814ef7c271b3890bbf270
maxappdev <maxym.oliynyk95@gmail.com>
2020-11-21 17:07:23 +0100

    fonts + sounds restored

--- src/sounds/AudioObject.js
+++ src/sounds/AudioObject.js
@@ -1,19 +1,19 @@
-//import {Audio} from "expo-av";
+import {Audio} from "expo-av";
 
 export let AudioObject = (function () {
     let instance;
 
     function createInstance() {
-        //let object = new Audio.Sound();
-        //return object;
+        let object = new Audio.Sound();
+        return object;
     }
 
     return {
         getInstance: function () {
-            //if (!instance) {
-              //  instance = createInstance();
-            //}
-            //return instance;
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
         }
     };
 })();
